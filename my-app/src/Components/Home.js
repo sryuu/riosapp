0bb71aa0f0ee2488672cfc8173857f296a84fbf5
劉志和 <ryuus@novelworks.jp>
2022-11-13 19:07:06 +0900

    change 4 to 2

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -1,45 +1,44 @@
 import {auth} from "../Server/firebase";
 import './Home.css';
 
-
 function Home () {
-    //logout from firebase
-    const logout = () => {
-        auth.signOut();
-    }
+  //logout from firebase
+  const logout = () => {
+    auth.signOut();
+  }
 
-    return (
-        <div className="Home">
-                Make your AIs by your hands
-            <div className="ChooseTypeOfAI">
-                <div className="TypeOfImageAI" id="GenresOfAI">
-                    <h1>Input Image</h1>
-                    <button className="GenresOfAIs">Image Classification</button>
-                    <button className="GenresOfAIs">Object Detection</button>
-                    <button className="GenresOfAIs">Semantic Segmentation</button>
-                    <button className="GenresOfAIs">Face Recognition</button>
-                    <button className="GenresOfAIs">Text Recognition</button>
-                    <button className="GenresOfAIs">Image To Image</button>
-                    <button className="GenresOfAIs">Semantic Segmentation To Image</button>
-                </div>
-                <div className="TypeOfTextAI" id="GenresOfAI">
-                    <h1>Input Text</h1>
-                    <button className="GenresOfAIs">Translate</button>
-                    <button className="GenresOfAIs">Summarize</button>
-                    <button className="GenresOfAIs">Text Generation</button>
-                    <button className="GenresOfAIs">Text Classification</button>
-                    <button className="GenresOfAIs">Chat Bot</button>
-                    <button className="GenresOfAIs">Text To Image</button>
-                </div>
-                <div className="TypeOfSoundAI" id="GenresOfAI">
-                    <h1>Input Sound</h1>
-                    <button className="GenresOfAIs">Classification</button>
-                    <button className="GenresOfAIs">Speech To Text</button>
-                </div>
-            </div>
-            <button className = "logout" onClick={logout}>Logout</button>
+  return (
+    <div className="Home">
+    Make your AIs by your hands
+      <div className="ChooseTypeOfAI">
+        <div className="TypeOfImageAI" id="GenresOfAI">
+          <h1>Input Image</h1>
+          <button className="GenresOfAIs">Image Classification</button>
+          <button className="GenresOfAIs">Object Detection</button>
+          <button className="GenresOfAIs">Semantic Segmentation</button>
+          <button className="GenresOfAIs">Face Recognition</button>
+          <button className="GenresOfAIs">Text Recognition</button>
+          <button className="GenresOfAIs">Image To Image</button>
+          <button className="GenresOfAIs">Semantic Segmentation To Image</button>
         </div>
-    )
+        <div className="TypeOfTextAI" id="GenresOfAI">
+          <h1>Input Text</h1>
+          <button className="GenresOfAIs">Translate</button>
+          <button className="GenresOfAIs">Summarize</button>
+          <button className="GenresOfAIs">Text Generation</button>
+          <button className="GenresOfAIs">Text Classification</button>
+          <button className="GenresOfAIs">Chat Bot</button>
+          <button className="GenresOfAIs">Text To Image</button>
+        </div>
+        <div className="TypeOfSoundAI" id="GenresOfAI">
+          <h1>Input Sound</h1>
+          <button className="GenresOfAIs">Classification</button>
+          <button className="GenresOfAIs">Speech To Text</button>
+        </div>
+      </div>
+      <button className = "logout" onClick={logout}>Logout</button>
+    </div>
+  )
 }
 
 export default Home
--- my-app/src/Components/Login.js
+++ my-app/src/Components/Login.js
@@ -3,44 +3,44 @@ import {auth} from "../Server/firebase";
 import './Login.css';
 
 import { 
-    GoogleAuthProvider , 
-    signInWithPopup , 
-    signInWithEmailAndPassword} 
+  GoogleAuthProvider , 
+  signInWithPopup , 
+  signInWithEmailAndPassword} 
 from "firebase/auth";
 
 const provider = new GoogleAuthProvider();
 
 function Login () {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-        <div className="pageLayout">
-            <div className="title">
-                Login
-            </div>
-            <div className="login">
-                <div className="loginWithEmailAndPass">
-                    email
-                    <input 
-                        type="text"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    password
-                    <input 
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <div className="Buttons">
-                    <button onClick= {() => signInWithEmailAndPassword(email, password)}>Login</button>
-                    <button onClick= {() => signInWithPopup(auth,provider)}>Login with Google</button>
-                </div>
-            </div>
+  return (
+    <div className="pageLayout">
+      <div className="title">
+        Login
+      </div>
+      <div className="login">
+        <div className="loginWithEmailAndPass">
+          email
+          <input 
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          password
+          <input 
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-    )
+        <div className="Buttons">
+          <button onClick= {() => signInWithEmailAndPassword(email, password)}>Login</button>
+          <button onClick= {() => signInWithPopup(auth,provider)}>Login with Google</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login
